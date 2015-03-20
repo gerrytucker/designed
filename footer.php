9fe748cc8b7dfa38d207b794cfae9ecdcbfcaa78
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-20 00:36:11 +0000

    Auto-generated commit 20/03/2015 12:36:03 AM

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 
-		<footer class="page-footer">
+		<footer class="page-footer grey darken-4">
 			<div class="container">
 				<div class="row">
 					<div class="col l4 s12">
-						<p>Designer, Web Designer, Bald</p>
+						<p class="white-text">Designer, Web Designer, Bald</p>
 					</div>
 					<div class="col l4 s12">
 					</div>
 					<div class="col l4 s12">
+						<p class="white-text">
 						<?php
 						$email = "designed@markwalling.co.uk";
 						if(function_exists('eos_obfuscate')) {
@@ -16,6 +17,7 @@
 							echo $email;
 						}
 						?>
+						</p>
 					</div>
 				</div>
 			</div>
