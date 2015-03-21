a359ecc677471aaef17ac76106d6b5d5fa32db06
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-21 17:44:20 +0000

    Auto-generated commit 21/03/2015 5:44:12 PM

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 		<section class="navbar">
 			<div class="container">
 				<div class="row">
-					<div class="col m4"><a class="brand" href="<?php echo get_bloginfo('home_url'); ?>" title="Mark Walling" rel="homepage">Mark Walling</a></div>
+					<div class="col m4"><a class="brand" href="<?php echo get_bloginfo('url'); ?>" title="Mark Walling" rel="homepage">Mark Walling</a></div>
 					<div class="col m8"></div>
 				</div>
 			</div>
