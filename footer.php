f575f886c2ff0bb53bc9e7a03d7854e7193c7c4c
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-22 15:12:25 +0000

    Auto-generated commit 22/03/2015 3:12:16 PM

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 
 		<section class="social grey lighten-5">
 			<div class="container">
-				<div class="row hide-on-small-and-down">
-					<div class="col m2">
+				<div class="row hide-on-small-only">
+					<div class="col m2 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-twitter"></span></a>
 					</div>
 					<div class="col m2 center-align">
@@ -22,24 +22,24 @@
 					</div>
 				</div>
 				<div class="row show-on-small">
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-twitter"></span></a>
 					</div>
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-facebook"></span></a>
 					</div>
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-google-plus"></span></a>
 					</div>
 				</div>
 				<div class="row show-on-small">
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-instagram"></span></a>
 					</div>
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-pinterest"></span></a>
 					</div>
-					<div class="col m4 center-align">
+					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-youtube"></span></a>
 					</div>
 				</div>
@@ -49,10 +49,10 @@
 		<footer class="page-footer grey darken-4">
 			<div class="container">
 				<div class="row">
-					<div class="col m4 s12">
+					<div class="col s4 s12">
 						<p class="white-text">Designer, Web Designer, Bald</p>
 					</div>
-					<div class="col m4 offset-m4 s12">
+					<div class="col s4 offset-s4 s12">
 						<p>
 						<?php
 						$email = "designed@markwalling.co.uk";
