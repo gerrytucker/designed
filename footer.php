8b558a9b522d64f260ce7e275b1bc030e91d679d
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-22 19:02:37 +0000

    Auto-generated commit 22/03/2015 7:02:31 PM

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 						<a href="#"><span class="grey-text darken-4 icomoon-youtube"></span></a>
 					</div>
 				</div>
-				<div class="row hide-on-med-up">
+				<div class="row hide-on-med-and-up">
 					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-twitter"></span></a>
 					</div>
@@ -32,7 +32,7 @@
 						<a href="#"><span class="grey-text darken-4 icomoon-google-plus"></span></a>
 					</div>
 				</div>
-				<div class="row hide-on-med-up">
+				<div class="row hide-on-med-and-up">
 					<div class="col s4 center-align">
 						<a href="#"><span class="grey-text darken-4 icomoon-instagram"></span></a>
 					</div>
