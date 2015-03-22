7f4974d270bad2c54aad3f79c8acdb734de8478a
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-22 22:50:19 +0000

    Auto-generated commit 22/03/2015 10:50:08 PM

--- index.php
+++ index.php
@@ -12,4 +12,44 @@
 			</div>
 		</header>
 		
+		<section class="portfolio">
+
+			<div class="container">
+<?php
+$query = new WP_Query('cat_name=portfolio&order=DESC&orderby=date&posts_per_page=6');
+while ($query->have_posts()) : the_post();
+	$count++;
+	if ($count == 1) :
+?>
+			
+				<div class="row">
+
+<?php
+	endif;
+?>
+
+					<div class="col s12 m4">
+						<?php
+							the_post_thumbnail('medium', array('class' => 'responsive-img'));
+						?>
+					</div>
+<?php
+	if ($count == 1) :
+?>
+			
+				</div>
+
+<?php
+	endif;
+	
+	if ($count == 3) {
+		$count = 0;
+	}
+
+endwhile;
+?>
+			</div>
+			
+		</section>
+
 <?php get_footer(); ?>
