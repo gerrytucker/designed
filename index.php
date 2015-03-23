e6191f60fdf29aa150b4adfbddecceb293953451
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-23 00:03:00 +0000

    Auto-generated commit 23/03/2015 12:02:53 AM

--- index.php
+++ index.php
@@ -15,6 +15,28 @@
 		<section class="portfolio">
 
 			<div class="container">
+
+			<?php
+			$query = new WP_Query('cat_name=portfolio&order=DESC&orderby=date&posts_per_page=6');
+			while ($query->have_posts()) : $query->the_post();
+
+			<?php if ($count == 0) : ?>
+
+				<div class="row">
+
+			<?php endif; // $count == 0 ?>
+
+
+			<?php $count++; ?>
+
+			<?php if ($count == 3) : $count = 0; ?>
+
+				</div>
+
+			<?php endif; // $count == 0 ?>
+
+			<?php endwhile; // have_posts ?>
+
 			</div>
 			
 		</section>
--- temp.php
+++ temp.php
@@ -2,31 +2,21 @@
 <?php
 $query = new WP_Query('cat_name=portfolio&order=DESC&orderby=date&posts_per_page=6');
 while ($query->have_posts()) : $query->the_post();
-	$count++;
-	if ($count == 1) :
-?>
-			
-				<div class="row">
 
-<?php
-	endif;
-?>
+<?php if ($count == 0) : ?>
 
-					<div class="col s12 m4">
-						<?php
-							the_post_thumbnail('medium', array('class' => 'responsive-img'));
-						?>
-					</div>
-<?php
-	if ($count == 3) {
-		$count = 0;
-?>
-			
-				</div>
+	<div class="row">
+		
+<?php endif; // $count == 0 ?>
 
-<?php
-	endif;
-	
-endwhile;
-?>
+
+<?php $count++; ?>
+
+<?php if ($count == 3) : $count = 0; ?>
+
+	</div>
+		
+<?php endif; // $count == 0 ?>
+
+<?php endwhile; // have_posts ?>
 
