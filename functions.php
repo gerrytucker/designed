3c60250055a5435fa12a11afe99ab48680c9d1d7
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-23 01:36:15 +0000

    Auto-generated commit 23/03/2015 1:36:09 AM

--- functions.php
+++ functions.php
@@ -2,6 +2,48 @@
 
 add_theme_support( 'post-thumbnails' );
 
+function portfolio_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Portfolio', 'Post Type General Name', 'portfolio' ),
+		'singular_name'       => _x( 'Portfolio', 'Post Type Singular Name', 'portfolio' ),
+		'menu_name'           => __( 'Portfolio', 'portfolio' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'portfolio' ),
+		'all_items'           => __( 'All Items', 'portfolio' ),
+		'view_item'           => __( 'View Portfolio Item', 'portfolio' ),
+		'add_new_item'        => __( 'Add Portfolio Item', 'portfolio' ),
+		'add_new'             => __( 'Add New', 'portfolio' ),
+		'edit_item'           => __( 'Edit Portfolio Item', 'portfolio' ),
+		'update_item'         => __( 'Update Portfolio Item', 'portfolio' ),
+		'search_items'        => __( 'Search Portfolio Item', 'portfolio' ),
+		'not_found'           => __( 'Not found', 'portfolio' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'portfolio' ),
+	);
+	$args = array(
+		'label'               => __( 'portfolio', 'portfolio' ),
+		'description'         => __( 'Portfolio', 'portfolio' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'portfolio', $args );
+
+}
+add_action( 'init', 'portfolio_post_type', 0 );
+
+
 function change_user_contact_information( $fields )
 {
     unset($fields['aim']);
