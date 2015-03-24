c77bdc6078fef2abed04b420b59ba3c6a0657c7b
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-24 21:28:28 +0000

    Auto-generated commit 24/03/2015 9:28:23 PM

---
+++ single-work.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+		<header class="valign-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col s10 offset-s1 center-align">
+					</div>
+				</div>
+			</div>
+		</header>
+		
+<?php get_footer(); ?>
