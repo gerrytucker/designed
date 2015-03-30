99cde8a246ad42efe728cff48840365bb28cfb89
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-30 21:11:13 +0100

    Auto-generated commit 30/03/2015 9:11:07 PM

--- category-portfolio.php
+++ category-portfolio.php
@@ -4,7 +4,6 @@
 			<div class="container">
 				<div class="row">
 					<div class="col s12 m10 offset-m1 center-align">
-						<h1><?php echo the_title(); ?></h1>
 					</div>
 				</div>
 			</div>
