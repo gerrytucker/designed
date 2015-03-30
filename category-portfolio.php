69b7ce59229c776d08444f3849d89d03b6adf5e6
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-30 13:22:03 +0100

    Auto-generated commit 30/03/2015 1:21:57 PM

---
+++ category-portfolio.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+		<header class="valign-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col s12 m10 offset-m1 center-align">
+						<h1><?php echo the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</header>
+		
+<?php get_footer(); ?>
