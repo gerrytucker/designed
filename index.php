8fac9398e5f8ff925850dc98e3e8caa11d9dfefc
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-04-03 11:32:11 +0100

    Auto-generated commit 03/04/2015 11:32:04 AM

--- index.php
+++ index.php
@@ -16,40 +16,6 @@
 
 			<div class="container">
 
-			<?php
-				$count = 0;
-				$query = new WP_Query('post_type=work&order=DESC&orderby=date&posts_per_page=-1');
-				while ($query->have_posts()) : $query->the_post();
-			?>
-
-			<?php if ($count == 0) : ?>
-
-				<div class="row">
-
-			<?php endif; // $count == 0 ?>
-
-				<div class="job col s12 m4">
-				
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium', array('class' => 'responsive-img')); ?></a>
-					
-				</div><!-- .job.col -->
-					
-			<?php $count++; ?>
-
-			<?php if ($count == 3) : $count = 0; ?>
-
-				</div><!-- .row -->
-
-			<?php endif; // $count == 0 ?>
-
-			<?php endwhile; // have_posts ?>
-
-			<?php if ($count < 3) : $count = 0; ?>
-
-				</div><!-- .row -->
-
-			<?php endif; // $count == 0 ?>
-
 			</div><!-- .container -->
 			
 		</section>
